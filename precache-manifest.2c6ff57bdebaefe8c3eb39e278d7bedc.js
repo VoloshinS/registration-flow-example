b3e0ac2f6a641a4dc22337405b1724ec393571b8
sergey.voloshin <sergey.voloshin@nextiva.com>
2020-02-06 13:44:13 +0200

    Updates

---
+++ precache-manifest.2c6ff57bdebaefe8c3eb39e278d7bedc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad7143ecf21c44b568c0f7f51c4ad79",
+    "url": "/registration-flow-example/index.html"
+  },
+  {
+    "revision": "57ee0c63d8c9bb78776e",
+    "url": "/registration-flow-example/static/css/main.c8396fb1.chunk.css"
+  },
+  {
+    "revision": "957d0af3dcef8e06947e",
+    "url": "/registration-flow-example/static/js/2.bb2f129e.chunk.js"
+  },
+  {
+    "revision": "a56dae6e0594ed0865c2bd6e9c670cf8",
+    "url": "/registration-flow-example/static/js/2.bb2f129e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57ee0c63d8c9bb78776e",
+    "url": "/registration-flow-example/static/js/main.0de678cf.chunk.js"
+  },
+  {
+    "revision": "74306a03f0893cdbc442",
+    "url": "/registration-flow-example/static/js/runtime-main.07ddb4a0.js"
+  }
+]);

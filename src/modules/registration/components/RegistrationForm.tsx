45c0d4508a4f219994d13c47beaebb829605a756
sergey.voloshin <sergey.voloshin@nextiva.com>
2020-02-09 21:13:24 +0200

    Moved form logic to custom hooks

--- src/modules/registration/components/RegistrationForm.tsx
+++ src/modules/registration/components/RegistrationForm.tsx
@@ -1,41 +1,21 @@
-import React, { FC, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import * as Yup from 'yup';
 
 import { Form } from '../../core';
-import { addUser, getUserError } from '../../core/redux/user.duck';
-import { User } from '../../core/interfaces';
-
-export const RegistrationSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  password: Yup.string()
-    .required('No password provided.')
-    .min(8, 'Password is too short - should be 8 chars minimum.')
-    .matches(/\S/, 'Password can only contain non whitespace character.'),
-});
+import { getUserError } from '../../core/redux/user.duck';
+import useRegistrationForm from '../hooks/useRegistrationForm';
 
 const RegistrationForm: FC = () => {
-  const dispatch = useDispatch();
   const error = useSelector(getUserError);
-
-  const handleSubmit = useCallback(
-    (user: User) => {
-      dispatch(addUser.request(user));
-    },
-    [dispatch]
-  );
+  const formik = useRegistrationForm();
 
   return (
     <Form
       icon={<PersonAddIcon />}
       title="Registration"
       buttonLabel="Register"
-      onSubmit={handleSubmit}
-      validationSchema={RegistrationSchema}
+      formik={formik}
       error={error}
     />
   );
